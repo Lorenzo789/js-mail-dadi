e619961eead14337f5c4838a0d497c2231bc5db6
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-06-17 14:55:15 +0200

    test

--- js/myscript.js
+++ js/myscript.js
@@ -8,4 +8,17 @@ const userMail = prompt('inserisci la tua mail');
 
 const emailAccess = ["luigi@gmail.com" , "riccardo@gmail.com" , "simone@gmail.com" , "lorenzo@gmail.com" , "tiziano@libero.it" ,
  "francesco@libero.it" , "luca@libero.it" , "vincenzo@yahoo.it" , "chiara@yahoo.it" , "eleonora@yahoo.it"] 
- 
+
+ for (let index = 0; index < emailAccess.length; index++) {
+
+    if (userMail == emailAccess[index]) {
+
+        console.log("accesso consentito");
+
+    } else {
+
+        console.log('accesso non consentito!! ritenta');
+
+    }
+    
+ }
