d29a1226944be18da2d3db4b57fb3e4c9d4d9d0d
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-06-17 14:53:59 +0200

    start

---
+++ js/myscript.js
@@ -0,0 +1,11 @@
+//chiedere all'utente con un prompt la sua mail
+
+//creare un array con una lista di mail concesse    
+
+//stampare un messaggio in console con un "accesso consentito"
+
+const userMail = prompt('inserisci la tua mail');
+
+const emailAccess = ["luigi@gmail.com" , "riccardo@gmail.com" , "simone@gmail.com" , "lorenzo@gmail.com" , "tiziano@libero.it" ,
+ "francesco@libero.it" , "luca@libero.it" , "vincenzo@yahoo.it" , "chiara@yahoo.it" , "eleonora@yahoo.it"] 
+ 
