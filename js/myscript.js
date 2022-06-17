b6cc5102f2e6b8f773faa706d245e0bcec0aff4c
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-06-17 15:05:20 +0200

    add comment

--- js/myscript.js
+++ js/myscript.js
@@ -1,13 +1,14 @@
 //chiedere all'utente con un prompt la sua mail
-
 //creare un array con una lista di mail concesse    
-
 //stampare un messaggio in console con un "accesso consentito"
 
+//ASK USER FOR HIS/HER EMAIL
 const userMail = prompt('inserisci la tua mail');
 
+//MESSAGE IN CASE THE EMAIL IS WRONG
 let message = ('accesso non consentito!! ritenta');
 
+//LIST EMAIL ACCESS FREE
 const emailAccess = ["luigi@gmail.com" , "riccardo@gmail.com" , "simone@gmail.com" , "lorenzo@gmail.com" , "tiziano@libero.it" ,
  "francesco@libero.it" , "luca@libero.it" , "vincenzo@yahoo.it" , "chiara@yahoo.it" , "eleonora@yahoo.it"] 
 
